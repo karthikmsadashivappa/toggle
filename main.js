9e0fb7cfbce0c7c38d64340fa6478a12c4527a37
Karthik M S <ms18karthik@gmail.com>
2018-01-05 23:42:45 +0530

    Looping through the password

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 document.addEventListener('click', function(event){
+
 	if (!event.target.hasAttribute('data-show-password')) return;
 	
-	var password = document.querySelector(event.target.getAttribute('data-show-password'));
-	if(!password) return;
+	var password = document.querySelectorAll(event.target.getAttribute('data-show-password'));
+	if(!password.length < 1) return;
+
+	for (var i=0; i<password.length; i++) {
 
 	if(event.target.checked) {
 	password.type='text';
 	} else {
 	password.type='password';
-		}
+	}
+	}
 }, false);
