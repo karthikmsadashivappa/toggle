4bb0315defe4bc1d501b90ca822934098324cd7f
Karthik M S <ms18karthik@gmail.com>
2018-01-06 00:14:56 +0530

    Added full funtionality to password

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ document.addEventListener('click', function(event){
 	if (!event.target.hasAttribute('data-show-password')) return;
 	
 	var password = document.querySelectorAll(event.target.getAttribute('data-show-password'));
-	if(!password.length < 1) return;
+	if(password.length < 1) return;
 
 	for (var i=0; i<password.length; i++) {
 
 	if(event.target.checked) {
-	password.type='text';
+	password[i].type='text';
 	} else {
-	password.type='password';
+	password[i].type='password';
 	}
 	}
 }, false);
